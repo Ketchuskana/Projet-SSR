43091365838cb473610f72a3d51ca74ee41a1c0e
Axelle-LM <98803198+Axelle-LM@users.noreply.github.com>
2025-01-30 11:19:53 +0100

    Update tailwind

--- my-app/src/app/projet/[id]/page.tsx
+++ my-app/src/app/projet/[id]/page.tsx
@@ -26,40 +26,56 @@ export default async function ProjetDetailsPage({ params }: Props) {
     }
 
     return (
-        <div className="p-4">
-            <h1 className="text-3xl font-bold">{projet.name}</h1>
-            <Image
-                src={projet.images} 
-                width={400}
-                height={300}
-                alt={projet.name}
-                className="rounded-lg my-4"
-            />
-            
-            <p>
-                <strong>Name :</strong> {projet.name}
-            </p>
-            <p>
-                <strong>Technologies :</strong> {projet.technologies}
-            </p>
-            <p>
-                <strong>Contexte :</strong> {projet.contexte}
-            </p>
-            <p>
-                <strong>Objectifs :</strong> {projet.objectifs}
-            </p>
-            <p>
-                <strong>Client :</strong> {projet.client}
-            </p>
-            <p>
-                <strong>Resultats :</strong> {projet.resultats}
-            </p>
-            <p>
-                <strong>Testimonials :</strong> {projet.testimonials}
-            </p>
-            <p>
-                <strong>Videos :</strong> {projet.videos}
-            </p>
+        <div className="p-6 lg:p-12 bg-gray-50">
+
+            <div className="mb-20">
+                <h1 className=".text-6xl lg:text-5xl text-gray-900 font-sans">{projet.name}</h1>
+                <p className="text-lg text-gray-500">
+                    <strong className="text-lg font-semibold text-gray-700">Client :</strong> {projet.client}
+                </p>
+            </div>
+
+
+            <div className="flex flex-col lg:flex-row items-center lg:items-start gap-12">
+
+                <div className="w-full lg:w-1/2 flex justify-center">
+                    <Image
+                        src="/download.jpg"
+                        width={400}
+                        height={300}
+                        alt={projet.name}
+                        className="rounded-xl shadow-lg"
+                    />
+                </div>
+
+                <div className="grid grid-cols-2 gap-y-4">
+                    <div className="text-lg font-semibold text-gray-700">Contexte</div>
+                    <div className="text-lg text-gray-500">{projet.contexte}</div>
+
+                    <div className="text-lg font-semibold text-gray-700">Objectifs</div>
+                    <div className="text-lg text-gray-500">{projet.objectifs}</div>
+
+                    <div className="text-lg font-semibold text-gray-700">Technologies</div>
+                    <div className="text-lg text-gray-500">{projet.technologies}</div>
+
+                    <div className="text-lg font-semibold text-gray-700">Résultats</div>
+                    <div className="text-lg text-gray-500">{projet.resultats}</div>
+
+                    {projet.testimonials && (
+                        <>
+                            <div className="text-lg font-semibold text-gray-700">Témoignages</div>
+                            <div className="text-lg text-gray-500">{projet.testimonials}</div>
+                        </>
+                    )}
+
+                    {projet.videos && (
+                        <>
+                            <div className="text-lg font-semibold text-gray-700">Vidéos</div>
+                            <div className="text-lg text-gray-500">{projet.videos}</div>
+                        </>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
